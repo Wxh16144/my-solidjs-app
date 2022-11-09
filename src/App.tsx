5ce8febe64b263d65b77c9a4ccb872f66f355d3e
wuxh <wxh1220@gmail.com>
2022-11-09 13:53:51 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import type { Component } from 'solid-js';
+
+
+const App: Component = () => {
+  return (
+      <header>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+  );
+};
+
+export default App;
